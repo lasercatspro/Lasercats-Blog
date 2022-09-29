4caad037a503b2656e875d4d18630e44437333db
societe-ecran <antoninreigneaud@gmail.com>
2022-09-29 11:49:16 +0200

    :memo: article cit ecriture collaborative + review

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -21,7 +21,7 @@ function Article({ article }) {
           {formatDate(article.date)}
         </Card.Eyebrow>
         <Card.Description>{article.description}</Card.Description>
-        <Card.Cta>Read article</Card.Cta>
+        <Card.Cta>Lire l&apos;article</Card.Cta>
       </Card>
       <Card.Eyebrow
         as="time"
@@ -46,7 +46,7 @@ export default function ArticlesIndex({ articles }) {
       </Head>
       <SimpleLayout
         title="What's new?"
-        intro="Tips, nouveau SaaS, design innovant, nouvelle techno... Les actualités qui nous ont attiré l'oeil et qu'on veut partager."
+        intro=" Au cours de nos dev', certains trouvailles et réalisations méritent d'étre partagées. Ici vous trouverez une série darticle pour faire vivre publiquement nos réflexions et travaux, que ce soit sur des sujets technique, dinterfaces ou dusages."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
